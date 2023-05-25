009d424a47a89899bc982d0d6f4a69b00ecee394
Ishika Goyal <99865224+ishikagoyal02@users.noreply.github.com>
2023-05-25 12:55:09 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Power of 2/power-of-2.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        if(n==0)
+            return false;
+            
+        if((n & (n-1))==0)
+            return true;
+        return false;    
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+
+        Solution ob;
+         if(ob.isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
