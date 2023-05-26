c4cd407c41822ee752b2825a2a167bded7c17d9f
Ishika Goyal <99865224+ishikagoyal02@users.noreply.github.com>
2023-05-26 18:49:30 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int setBit(int N)
+    {
+        if((N & (N+1))==0)
+            return N;
+        int x=N+1;
+        return x|N;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans = ob.setBit(N);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
