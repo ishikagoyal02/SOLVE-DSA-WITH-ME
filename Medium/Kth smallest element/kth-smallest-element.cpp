3692dbe7615bc4044ac09465c8050ec198d09639
Ishika Goyal <99865224+ishikagoyal02@users.noreply.github.com>
2023-06-29 08:23:09 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kth smallest element/kth-smallest-element.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        
+        priority_queue<int,vector<int>,greater<int>> q;
+        
+        for(int i=l;i<=r;i++)
+            q.push(arr[i]);
+            
+        for(int i=1;i<k;i++)
+            q.pop();
+            
+        return q.top();        
+    }
+};
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
