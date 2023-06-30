3dfc4f648b93d456d6d1924983b009f4643c4a46
Ishika Goyal <99865224+ishikagoyal02@users.noreply.github.com>
2023-06-30 10:16:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Merge two binary Max heaps/merge-two-binary-max-heaps.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// C++ program to merge two max heaps.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        // your code here
+        
+        priority_queue<int> ans;
+        vector<int> v;
+        
+        for(int i=0;i<a.size();i++)
+            ans.push(a[i]);
+        for(int i=0;i<b.size();i++)
+            ans.push(b[i]);
+        
+        while(!ans.empty())
+        {
+             v.push_back(ans.top());
+             ans.pop();
+        }
+        
+        return v;    
+    }
+};
+
+//{ Driver Code Starts.
+
+bool isMerged(vector<int> &arr1, vector<int> &arr2, vector<int> &merged){
+    if (arr1.size() + arr2.size() != merged.size()){
+        return false;
+    }
+    arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+    sort(arr1.begin(),arr1.end());
+    vector<int> mergedCopy =merged;
+    sort(mergedCopy.begin(),mergedCopy.end());
+    if (arr1 != mergedCopy){
+        return false;
+    }
+    for(int i = 1; i<merged.size(); i++){
+        if(merged[i] > merged[(i-1)/2])
+            return false;
+    }
+    return true;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i;
+        cin >> n >> m;
+        vector<int> a(n,0),b(m,0);
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (i = 0; i < m; i++) {
+            cin >> b[i];
+        }
+        vector<int> merged, copyA = a, copyB = b;
+        Solution obj;
+        merged = obj.mergeHeaps(a, b, n, m);
+        bool flag = isMerged(copyA, copyB, merged);
+        if(flag == false) cout<<0<<endl;
+        else cout<<1<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
