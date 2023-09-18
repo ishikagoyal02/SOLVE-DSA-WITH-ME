bf869596d463406ab80d98a8ed9546995614b82e
Ishika Goyal <99865224+ishikagoyal02@users.noreply.github.com>
2023-09-18 14:53:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remaining String/remaining-string.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	string printString(string &S, char ch, int count)
+	{
+	    // Your code goes here
+
+	    int j=0;
+	    while(count && j<S.size())
+	    {
+	        if(S[j]==ch)
+	            count--;
+	            
+	        j++;
+	    }
+	    
+	    S.erase(0,j);
+	    
+	    if(S.size()==0)
+	        return "Empty string";
+	    return S;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		char ch;
+   		int count;
+
+   		cin >> s >> ch >> count;
+   		Solution ob;
+   		cout << ob.printString(s, ch, count) << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
